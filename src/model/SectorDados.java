24cee014b724781fe2151cb34473d9bf0acff9b5
JuanMajin <juan.majin@correounivalle.edu.co>
2022-01-16 00:53:23 -0500

    Se agregan las clases Dados y la clase SectorDados

--- src/model/SectorDados.java
+++ src/model/SectorDados.java
@@ -1,2 +1,40 @@
-package model;public class SectorDados {
+package model;
+
+import java.util.ArrayList;
+
+public class SectorDados {
+
+    private String nombre;
+    private ArrayList<Dado> dados;
+
+    public SectorDados(String nombreSector, int numeroDados) {
+        nombre = nombreSector;
+        dados = new ArrayList<Dado>(numeroDados);
+        for (int i = 0; i < numeroDados; i++) {
+            dados.add(new Dado());
+        }
+    }
+
+    public ArrayList<Dado> getDados() {
+        return dados;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public SectorDados moverDado(Dado dado, SectorDados sectorActual, SectorDados sectorFinal) {
+        sectorActual.getDados().remove(dado);
+        sectorFinal.getDados().add(dado);
+        return sectorFinal;
+    }
+
+    public void setDados(ArrayList<Dado> dados) {
+        this.dados = dados;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
